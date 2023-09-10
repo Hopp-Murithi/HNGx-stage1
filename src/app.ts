878ec2db03c4dead5666c9522532d95d9a842e8b
Hopp-Murithi <hoppmurithi@gmail.com>
2023-09-10 12:25:04 +0300

    feat: mount routes

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,20 @@
-import express, { Response, NextFunction } from "express";
+import express from "express";
+import {user} from './routes/info.route';
+
+
 
 export const app = express();
+
 app.use(express.json());
-console.log(process.env.PORT)
+app.use(express.urlencoded({ extended: true }));
+/**
+ * stageOne base url
+ */
+const stageOneURL = "/task/v1";
+
+
+
+/**
+ * mount routes
+ */
+app.use(stageOneURL,user);
